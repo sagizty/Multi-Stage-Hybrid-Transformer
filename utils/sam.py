493b529c3285b5e30c50edb7d960df7e752d1d27
lsqqqq <39699603+lsqqqq@users.noreply.github.com>
2021-10-30 18:45:53 +0100

    Update sam.py

--- utils/sam.py
+++ utils/sam.py
@@ -1,51 +1,51 @@
 """
-版本： 8月26日 17：00
-SAM范化性训练，避免过拟合的优化器优化工具  ICLR 2021 spotlight paper by Google
+ver： AUG 26th 17：00 official release
+SAM normalized training, optimizer optimization tool to avoid over-fitting  ICLR 2021 spotlight paper by Google
 
-介绍：https://mp.weixin.qq.com/s/04VT-ldd0-XEkhEW6Txl_A
-第三方实现来自：https://pub.towardsai.net/we-dont-need-to-worry-about-overfitting-anymore-9fb31a154c81
+Introduction：https://mp.weixin.qq.com/s/04VT-ldd0-XEkhEW6Txl_A
+Third party implementation from：https://pub.towardsai.net/we-dont-need-to-worry-about-overfitting-anymore-9fb31a154c81
 
-论文：Sharpness-aware Minimization for Efficiently Improving Generalization
-链接：https://arxiv.org/abs/2010.01412
+Paper：Sharpness-aware Minimization for Efficiently Improving Generalization
+Link：https://arxiv.org/abs/2010.01412
 
-计算原理：
-在训练过程中，优化器更新模型参数w时，整体上可以分为四个步骤：
+Calculation principle:
+During training, when the optimizer updates the model parameter w, it can be divided into four steps as a whole:
 
-（1）基于参数 w 对 batch data S 计算 gradient G 。
+ (1) Calculate gradient G on batch data S based on parameter w.
+ 
+ (2) Solve the dual norm of G, update parameters according to the direction of dual vector, and obtain the auxiliary model under the w+ε system.
 
-（2）求解 G 的 dual norm，依照 dual vector 方向更新参数，得到 w+ε体系下的辅助模型。
+ (3) Based on the auxiliary model under the parameter w+ε, calculate gradient G' for S
+ 
+ (4) Update original model parameter w with G'
 
-（3）基于参数 w+ε 下的辅助模型，对 S 计算 gradient G’ 。
-
-（4）用 G’ 更新原本的模型的参数 w 。
-
-
-使用例子：
+Usage examples:
 from sam import SAM
 ...
 model = YourModel()
-base_optimizer = torch.optim.SGD  # 传入一个优化器模板
-optimizer = SAM(model.parameters(), base_optimizer, lr=0.1, momentum=0.9)  # 优化器参数
+base_optimizer = torch.optim.SGD  # Pass in an optimizer template
+optimizer = SAM(model.parameters(), base_optimizer, lr=0.1, momentum=0.9)  # Optimizer parameters
 ...
 for input, output in data:
 
-  # first forward-backward pass，计算第一轮loss，这个和普通的一样
-  # 第一轮的loss是真实模型跑出来的，我们统计中需要的loss就是它，第二轮loss不是真实的模型的loss（是辅助模型的），所以不需要用在传统统计loss中
+  # first forward-backward pass, Calculate first round loss, which is the same as normal
+  # The loss in the first round is run out of the real model, which is what we need in statistics. 
+  # The loss in the second round is not the loss of the real model (it is fromn auxiliary model), so it does not need to be used in traditional statistical loss.
   output = model(input)
   loss = loss_function(output, labels)  # use this loss for any training statistics！！！！
-  loss.backward()  # 模型反向传播，记录原梯度。
+  loss.backward()  # Model back propagation, record original gradient.
 
-  # step1 的SAM类计算了“SAM梯度”
-  optimizer.first_step(zero_grad=True)  # 第一轮opt用“SAM梯度”对原模型参数体系进行了更新，现在模型变成了辅助模型，
-  # step1记录保存了回到原模型参数体系的变化方法
+  # SAM class from step1 calculated the 'SAM gradient'
+  optimizer.first_step(zero_grad=True)  # The first round of opt used "SAM gradient" to update the original model parameter system, and now the model has become an auxiliary model.
+  # Step1 records and saves the method of returning to the original model parameter system
 
-  # second forward-backward pass  第二轮先对辅助模型（step1更新后的模型）正向、反向传播
-  output2 = model(input)  # 用output2 确保计算图是辅助模型（即step1更新后的模型），不然有一堆bug。
+  # second forward-backward pass  In the second round, the auxiliary model (the updated model in step1) is propagated forward and backward first
+  output2 = model(input)  # Use output2 to ensure that the calculation graph is an auxiliary model (that is, the updated model of step1), otherwise there will be a bunch of bugs.
 
-  # 由于新增了计算图，因此计算时间增加显存占用也增加？
+  # Due to the newly added calculation graph, the calculation time and the memory usage increased?
 
-  loss_function(output2, labels).backward()  # make sure to do a full forward pass 辅助模型反向传播，记录更新梯度
-  optimizer.second_step(zero_grad=True)  # 第二轮，先原模型参数替换回去，之后base opt以辅助模型的更新方向对原模型参数体系进行更新
+  loss_function(output2, labels).backward()  # make sure to do a full forward pass. Auxiliary model backpropagation, record the updated gradient
+  optimizer.second_step(zero_grad=True)  # In the second round, replace the original model parameters first, and then base opt updates the original model parameter system with the update direction of the auxiliary model.
 ...
 
 
@@ -64,25 +64,25 @@ class SAM(torch.optim.Optimizer):
         self.param_groups = self.base_optimizer.param_groups
 
     @torch.no_grad()
-    def first_step(self, zero_grad=False):  # step1 生成辅助模型，对原模型参数进行修改把他变成辅助模型，同时记录怎么变的，以便还原
+    def first_step(self, zero_grad=False):  # step1 Generate auxiliary model, modify the original model parameters to turn it into an auxiliary model, and record how it changed so as to restore
         grad_norm = self._grad_norm()
 
         for group in self.param_groups:
-            scale = group["rho"] / (grad_norm + 1e-12)  # 附近的梯度影响
+            scale = group["rho"] / (grad_norm + 1e-12)  # Gradient influence nearby
 
             for p in group["params"]:
                 if p.grad is None:
                     continue
-                e_w = p.grad * scale.to(p)  # 考虑附近的梯度影响之后，确定辅助模型的参数变化需要的“SAM梯度”
-                p.add_(e_w)  # climb to the local maximum "w + e(w)"  inplace参数更新！ 因此是 原模型 变成了 辅助模型
+                e_w = p.grad * scale.to(p)  # After considering the influence of nearby gradients, determine the "SAM gradient" required for the parameter changes of the auxiliary model
+                p.add_(e_w)  # climb to the local maximum "w + e(w)". Inplace parameters updated! Thus original model changed into auxiliary model
                 self.state[p]["e_w"] = e_w
 
         if zero_grad:
             self.zero_grad()
 
     @torch.no_grad()
-    def second_step(self, zero_grad=False):  # step2 先对辅助模型参数进行修改把他变回原模型，
-        # 之后对原模型基于辅助模型的梯度用base_optimizer进行参数更新
+    def second_step(self, zero_grad=False):  # step2 First modify the auxiliary model parameters to change it back to the original model,
+        # After that, the gradient of the original model based on the auxiliary model is updated with base_optimizer.
 
         for group in self.param_groups:
 
@@ -90,7 +90,7 @@ class SAM(torch.optim.Optimizer):
                 if p.grad is None:
                     continue
                 p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
-                # 辅助模型参数还原，回到原模型 get back to "w" from "w + e(w)"，注意这个也是inplace的！！
+                # Auxiliary model parameter restoration, return to the original 'model get back to "w" from "w + e(w)"', note that this is also inplaced! !
 
         self.base_optimizer.step()  # 用base_optimizer对原模型进行参数更新 do the actual "sharpness-aware" update
 
